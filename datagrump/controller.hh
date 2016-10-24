919dfbe22fbe92e3f943f5a6f384a2a3f1dbb38a
Cenk Baykal <CenkBaykal@30-10-33.wireless.csail.mit.edu>
2016-10-24 13:51:51 -0400

    fix reward definition

--- datagrump/controller.hh
+++ datagrump/controller.hh
@@ -23,8 +23,8 @@ private:
     std::size_t arm_to_congestion_window(uint64_t arm);
     void compute_probabilities();
 
-    static const std::size_t MAX_WINDOW = 50;
-    static const std::size_t DELTA_WINDOW = 3;
+    static const std::size_t MAX_WINDOW = 40;
+    static const std::size_t DELTA_WINDOW = 5;
     static constexpr float G = 10000;
 
     bool debug_;

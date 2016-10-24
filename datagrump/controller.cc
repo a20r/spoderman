8db860b36d21440487e73bd1586d6bf55e7a76ef
Cenk Baykal <CenkBaykal@30-10-33.wireless.csail.mit.edu>
2016-10-24 16:24:02 -0400

    fix reward definition

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -56,8 +56,9 @@ void Controller::reset_weights() {
 }
 
 void Controller::reset_weights_low() {
+    double mid = K / 2;
     for (std::size_t i = 0; i < weights.size(); ++i) {
-        weights[i] = 1.0/(i + 1);
+        weights[i] = 1.0/exp(mid - i);
     }    
 }
 
